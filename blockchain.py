2476d37281a38bdd402c1b75c350e91332a8b1dc
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-09-13 21:30:00 +0100

    Fix docstrings

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-This is our Blockchain class which chains blocks via previous hashes.
-"""
+"""This is our Blockchain class which chains blocks via previous hashes."""
 
 from block import Block
 
@@ -11,20 +9,21 @@ class Blockchain:
         self.genesis_block()
     
     def genesis_block(self):
-        # This method initialises the blockchain with a unique block.
+        """Initialise the blockchain with a unique block."""
         self.chain.append(Block([], "0"))
         
     def add_block(self, transactions):
-        # This method allows new blocks to be added to the blockchain.
-        # We also allow nodes to verify Proof-of-Work via proof_nonce.
+        """Add new blocks to be added to the blockchain."""
         previous_hash = self.chain[-1].hash
         new_block = Block(transactions, previous_hash)
         self.chain.append(new_block)
+        
+        # Allows nodes to verify Proof-of-Work via proof_nonce.
         proof, proof_nonce = self.proof_of_work(new_block)
         return proof, proof_nonce, new_block
     
     def validate(self):
-        # This method allows for blockchain veracity to be checked.
+        """Check blockchain veracity."""
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
             previous_block = self.chain[i-1]
@@ -41,9 +40,12 @@ hash value stored in block {i}."""
         return True
     
     def proof_of_work(self, block, difficulty = 2):
-        # This method implements Proof-of-Work as a requirement before
-        # any blocks are added to the blockchain. Higher difficulties
-        # require more computational work.
+        """Implement Proof-of-Work prior to adding blocks.
+        
+        Arguments:
+        block -- the block to be added to the blockchain
+        difficulty -- higher difficulties need more computational work
+        """
         proof = block.generate_hash()
         while proof[:difficulty] != "0" * difficulty:
             block.nonce += 1
