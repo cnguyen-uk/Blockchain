b295949c548bb36b381ceacf615758ca24acd39b
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-09-13 21:46:14 +0100

    Fix comment spacing

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 This file demonstrates some of the capabilities of our blockchain
-implementation. In particular, we show how to:
+implementation.  In particular, we show how to:
 - Add blocks to the blockchain;
 - Print block information;
 - Print Proof-of-Work information to validate for yourself; and
@@ -28,7 +28,7 @@ mempool = [transaction1, transaction2, transaction3, transaction4,
            transaction5, transaction6, transaction7]
 
 # Blocks have a limited amount of space and so miners can only work on
-# a limited number of them at a time. This can cause heavy delays
+# a limited number of them at a time.  This can cause heavy delays
 # during times of peak popularity such as in 2018 with Bitcoin.
 block_transactions1 = mempool[:3]
 block_transactions2 = mempool[3:5]
@@ -38,7 +38,8 @@ block_transactions3 = mempool[5:7]
 blockchain = Blockchain()
 
 # To add transactions into a block, we simply use the add_block()
-# method. We never need to create a block from scratch.
+# method.  We never need to create a block from scratch.
+#
 # We also have the option of printing the add_block() information which
 # is particularly useful if we want to check the Proof-of-Work and the
 # successful nonce used to generate the Proof-of-Work.
@@ -52,7 +53,7 @@ print(blockchain.chain)
 print(blockchain.chain[2])
 
 # A small test case to demonstrate what happens if an attacker attempts
-# to change a previous transaction. In this case, we try to remove the
+# to change a previous transaction.  In this case, we try to remove the
 # existence of transaction3.
 blockchain.validate()
 blockchain.chain[1].transactions = [transaction1, transaction2]
